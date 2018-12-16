c6bfe167da66935f62a97f2cea01a34e947d2b8e
Yong-L <dkrcjs7521@gmail.com>
2018-12-16 17:42:57 -0500

    Build files

---
+++ react_app/build/precache-manifest.a62edc461ce233975de4e0eb513cf8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a0a6a7e9f84fa926b3d",
+    "url": "/static/js/main.3a0a6a7e.chunk.js"
+  },
+  {
+    "revision": "3945423a1a6b3198f19c",
+    "url": "/static/js/1.3945423a.chunk.js"
+  },
+  {
+    "revision": "3a0a6a7e9f84fa926b3d",
+    "url": "/static/css/main.037ac66a.chunk.css"
+  },
+  {
+    "revision": "3945423a1a6b3198f19c",
+    "url": "/static/css/1.58cd763c.chunk.css"
+  },
+  {
+    "revision": "57eeaa616589d1e07cf28603104f1a46",
+    "url": "/index.html"
+  }
+];
